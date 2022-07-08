97ff0e728490cc61421227fa9d268a234b9ba522
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-08 21:38:16 +0900

    channel

--- content.js
+++ content.js
@@ -65,13 +65,14 @@ import(chrome.runtime.getURL('lang/' + document.documentElement.attributes['lang
 			node.querySelectorAll('span.filter-button.channels_none').forEach(n => n.style.display = '');
 
 			node.querySelectorAll('span.filter-query').forEach(n => n.style.display = '');
-		} else if (window.location.href.startsWith('https://www.youtube.com/channel/') || window.location.href.startsWith('https://www.youtube.com/c/')) {
+		} else if (window.location.href.startsWith('https://www.youtube.com/channel/')
+			|| window.location.href.startsWith('https://www.youtube.com/c/')) {
 			node.querySelectorAll('span.filter-button.all').forEach(n => n.style.display = '');
-			node.querySelectorAll('span.filter-button.live').forEach(n => n.style.display = 'none');
-			node.querySelectorAll('span.filter-button.streamed').forEach(n => n.style.display = 'none');
-			node.querySelectorAll('span.filter-button.video').forEach(n => n.style.display = 'none');
-			node.querySelectorAll('span.filter-button.scheduled').forEach(n => n.style.display = 'none');
-			node.querySelectorAll('span.filter-button.notification_on').forEach(n => n.style.display = 'none');
+			node.querySelectorAll('span.filter-button.live').forEach(n => n.style.display = '');
+			node.querySelectorAll('span.filter-button.streamed').forEach(n => n.style.display = '');
+			node.querySelectorAll('span.filter-button.video').forEach(n => n.style.display = '');
+			node.querySelectorAll('span.filter-button.scheduled').forEach(n => n.style.display = '');
+			node.querySelectorAll('span.filter-button.notification_on').forEach(n => n.style.display = '');
 
 			node.querySelectorAll('span.filter-button.channels_all').forEach(n => n.style.display = 'none');
 			node.querySelectorAll('span.filter-button.channels_personalized').forEach(n => n.style.display = 'none');
