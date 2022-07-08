0bf81935148f4f5e5f99a747049d95d8b41e9e6e
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-09 08:42:15 +0900

    fil

---
+++ lang/fil-PH.js
@@ -0,0 +1,58 @@
+// div#metadata-line
+export function isLive_metadata(text) {
+    return text.includes('ang nanonood');
+}
+
+// div#metadata-line
+export function isStreamed_metadata(text) {
+    return text.includes('Nai-stream');
+}
+
+// div#metadata-line
+export function isVideo_metadata(text) {
+    return text.includes('panonood')
+        && !text.includes('Nai-stream');
+}
+
+// div#metadata-line
+export function isScheduled_metadata(text) {
+    return text.includes('Naka-iskedyul')
+        || text.includes('Magsisimula');
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isLive_status_label(text) {
+    return text === 'Live';
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isVideo_status_label(text) {
+    return text.endsWith('segundo')
+        || text.endsWith('minuto')
+        || text.endsWith('oras');
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOn_button(text) {
+    return text === 'Naka-on ang notification';
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOff_button(text) {
+    return text === 'Abisuhan ako';
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsAllNotifications(text) {
+    return text.includes('lahat ng notification');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsPersonalizedNotifications(text) {
+    return text.includes('Mga personalized na notification');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsNoNotifications(text) {
+    return text.includes('huwag tumanggap ng mga notification');
+}
