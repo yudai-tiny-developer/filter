636d94e75c9b6ccb27896cfcd35fa198ef11ccd4
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-09 07:36:56 +0900

    fr

---
+++ lang/fr-CA.js
@@ -0,0 +1,61 @@
+// div#metadata-line
+export function isLive_metadata(text) {
+    return text.includes('spectateur');
+}
+
+// div#metadata-line
+export function isStreamed_metadata(text) {
+    return text.includes('Diffusé');
+}
+
+// div#metadata-line
+export function isVideo_metadata(text) {
+    return text.includes('visionnement')
+        && !text.includes('Diffusé');
+}
+
+// div#metadata-line
+export function isScheduled_metadata(text) {
+    return text.includes('Planifié')
+        || text.includes('Première');
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isLive_status_label(text) {
+    return text === 'En direct';
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isVideo_status_label(text) {
+    return text.endsWith('seconde')
+        || text.endsWith('secondes')
+        || text.endsWith('minute')
+        || text.endsWith('minutes')
+        || text.endsWith('heure')
+        || text.endsWith('heures');
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOn_button(text) {
+    return text === 'Notifications activées';
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOff_button(text) {
+    return text === "Me prévenir";
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsAllNotifications(text) {
+    return text.includes('recevoir toutes les notifications.');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsPersonalizedNotifications(text) {
+    return text.includes('recevoir les notifications personnalisées.');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsNoNotifications(text) {
+    return text.includes('ne recevoir aucune notification.');
+}
---
+++ lang/fr-FR.js
@@ -0,0 +1,61 @@
+// div#metadata-line
+export function isLive_metadata(text) {
+    return text.includes('spectateur');
+}
+
+// div#metadata-line
+export function isStreamed_metadata(text) {
+    return text.includes('Diffusé');
+}
+
+// div#metadata-line
+export function isVideo_metadata(text) {
+    return text.includes('vue')
+        && !text.includes('Diffusé');
+}
+
+// div#metadata-line
+export function isScheduled_metadata(text) {
+    return text.includes('Planifié')
+        || text.includes('Première');
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isLive_status_label(text) {
+    return text === 'En direct';
+}
+
+// span#text.ytd-thumbnail-overlay-time-status-renderer[aria-label]
+export function isVideo_status_label(text) {
+    return text.endsWith('seconde')
+        || text.endsWith('secondes')
+        || text.endsWith('minute')
+        || text.endsWith('minutes')
+        || text.endsWith('heure')
+        || text.endsWith('heures');
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOn_button(text) {
+    return text === 'Notification activée';
+}
+
+// ytd-toggle-button-renderer
+export function isNotificationOff_button(text) {
+    return text === "M'avertir";
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsAllNotifications(text) {
+    return text.includes('Recevoir toutes les notifications');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsPersonalizedNotifications(text) {
+    return text.includes('Notifications personnalisées');
+}
+
+// ytd-subscription-notification-toggle-button-renderer button#button[aria-label]
+export function isChannelsNoNotifications(text) {
+    return text.includes('Aucune notification');
+}
