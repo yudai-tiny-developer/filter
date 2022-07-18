07beeeb4456456c17667c4c67bcbbc8640c01368
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-19 00:12:31 +0900

    layout

--- popup.js
+++ popup.js
@@ -10,6 +10,14 @@ const button_channels_all = chrome.i18n.getMessage('button_channels_all');
 const button_channels_personalized = chrome.i18n.getMessage('button_channels_personalized');
 const button_channels_none = chrome.i18n.getMessage('button_channels_none');
 
+function createRow(label, toggle) {
+    const div = document.createElement('div');
+    div.classList.add('row');
+    div.appendChild(label);
+    div.appendChild(toggle);
+    return div;
+}
+
 function createLabel(label) {
     const div = document.createElement('div');
     div.classList.add('label');
@@ -39,38 +47,17 @@ chrome.storage.local.get([
 ], (data) => {
     const mode_list = document.querySelector('div#mode_list');
     if (mode_list) {
-        mode_list.appendChild(createLabel(button_live));
-        mode_list.appendChild(createToggle('live', data.live, true));
-
-        mode_list.appendChild(createLabel(button_streamed));
-        mode_list.appendChild(createToggle('streamed', data.streamed, true));
-
-        mode_list.appendChild(createLabel(button_live_streamed));
-        mode_list.appendChild(createToggle('live_streamed', data.live_streamed, false));
-
-        mode_list.appendChild(createLabel(button_video));
-        mode_list.appendChild(createToggle('video', data.video, true));
-
-        mode_list.appendChild(createLabel(button_streamed_video));
-        mode_list.appendChild(createToggle('streamed_video', data.streamed_video, false));
-
-        mode_list.appendChild(createLabel(button_scheduled));
-        mode_list.appendChild(createToggle('scheduled', data.scheduled, true));
-
-        mode_list.appendChild(createLabel(button_notification_on));
-        mode_list.appendChild(createToggle('notification_on', data.notification_on, true));
-
-        mode_list.appendChild(createLabel(button_notification_off));
-        mode_list.appendChild(createToggle('notification_off', data.notification_off, false));
-
-        mode_list.appendChild(createLabel(button_channels_all));
-        mode_list.appendChild(createToggle('channels_all', data.channels_all, true));
-
-        mode_list.appendChild(createLabel(button_channels_personalized));
-        mode_list.appendChild(createToggle('channels_personalized', data.channels_personalized, true));
-
-        mode_list.appendChild(createLabel(button_channels_none));
-        mode_list.appendChild(createToggle('channels_none', data.channels_none, true));
+        mode_list.appendChild(createRow(createLabel(button_live), createToggle('live', data.live, true)));
+        mode_list.appendChild(createRow(createLabel(button_streamed), createToggle('streamed', data.streamed, true)));
+        mode_list.appendChild(createRow(createLabel(button_live_streamed), createToggle('live_streamed', data.live_streamed, false)));
+        mode_list.appendChild(createRow(createLabel(button_video), createToggle('video', data.video, true)));
+        mode_list.appendChild(createRow(createLabel(button_streamed_video), createToggle('streamed_video', data.streamed_video, false)));
+        mode_list.appendChild(createRow(createLabel(button_scheduled), createToggle('scheduled', data.scheduled, true)));
+        mode_list.appendChild(createRow(createLabel(button_notification_on), createToggle('notification_on', data.notification_on, true)));
+        mode_list.appendChild(createRow(createLabel(button_notification_off), createToggle('notification_off', data.notification_off, false)));
+        mode_list.appendChild(createRow(createLabel(button_channels_all), createToggle('channels_all', data.channels_all, true)));
+        mode_list.appendChild(createRow(createLabel(button_channels_personalized), createToggle('channels_personalized', data.channels_personalized, true)));
+        mode_list.appendChild(createRow(createLabel(button_channels_none), createToggle('channels_none', data.channels_none, true)));
     }
 
     for (const input of document.querySelectorAll('input.checkbox')) {
