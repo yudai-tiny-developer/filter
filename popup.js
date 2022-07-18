bd35e555c7d7e97c06b31afad650879c00f4aa0c
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2022-07-18 16:05:55 +0900

    DOMContentLoaded

--- popup.js
+++ popup.js
@@ -24,67 +24,65 @@ function createToggle(mode, setting, deafultValue) {
     return div;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.local.get([
-        'live',
-        'streamed',
-        'live_streamed',
-        'video',
-        'streamed_video',
-        'scheduled',
-        'notification_on',
-        'notification_off',
-        'channels_all',
-        'channels_personalized',
-        'channels_none'
-    ], (data) => {
-        const mode_list = document.querySelector('div#mode_list');
-        if (mode_list) {
-            mode_list.appendChild(createLabel(button_live));
-            mode_list.appendChild(createToggle('live', data.live, true));
+chrome.storage.local.get([
+    'live',
+    'streamed',
+    'live_streamed',
+    'video',
+    'streamed_video',
+    'scheduled',
+    'notification_on',
+    'notification_off',
+    'channels_all',
+    'channels_personalized',
+    'channels_none'
+], (data) => {
+    const mode_list = document.querySelector('div#mode_list');
+    if (mode_list) {
+        mode_list.appendChild(createLabel(button_live));
+        mode_list.appendChild(createToggle('live', data.live, true));
 
-            mode_list.appendChild(createLabel(button_streamed));
-            mode_list.appendChild(createToggle('streamed', data.streamed, true));
+        mode_list.appendChild(createLabel(button_streamed));
+        mode_list.appendChild(createToggle('streamed', data.streamed, true));
 
-            mode_list.appendChild(createLabel(button_live_streamed));
-            mode_list.appendChild(createToggle('live_streamed', data.live_streamed, false));
+        mode_list.appendChild(createLabel(button_live_streamed));
+        mode_list.appendChild(createToggle('live_streamed', data.live_streamed, false));
 
-            mode_list.appendChild(createLabel(button_video));
-            mode_list.appendChild(createToggle('video', data.video, true));
+        mode_list.appendChild(createLabel(button_video));
+        mode_list.appendChild(createToggle('video', data.video, true));
 
-            mode_list.appendChild(createLabel(button_streamed_video));
-            mode_list.appendChild(createToggle('streamed_video', data.streamed_video, false));
+        mode_list.appendChild(createLabel(button_streamed_video));
+        mode_list.appendChild(createToggle('streamed_video', data.streamed_video, false));
 
-            mode_list.appendChild(createLabel(button_scheduled));
-            mode_list.appendChild(createToggle('scheduled', data.scheduled, true));
+        mode_list.appendChild(createLabel(button_scheduled));
+        mode_list.appendChild(createToggle('scheduled', data.scheduled, true));
 
-            mode_list.appendChild(createLabel(button_notification_on));
-            mode_list.appendChild(createToggle('notification_on', data.notification_on, true));
+        mode_list.appendChild(createLabel(button_notification_on));
+        mode_list.appendChild(createToggle('notification_on', data.notification_on, true));
 
-            mode_list.appendChild(createLabel(button_notification_off));
-            mode_list.appendChild(createToggle('notification_off', data.notification_off, false));
+        mode_list.appendChild(createLabel(button_notification_off));
+        mode_list.appendChild(createToggle('notification_off', data.notification_off, false));
 
-            mode_list.appendChild(createLabel(button_channels_all));
-            mode_list.appendChild(createToggle('channels_all', data.channels_all, true));
+        mode_list.appendChild(createLabel(button_channels_all));
+        mode_list.appendChild(createToggle('channels_all', data.channels_all, true));
 
-            mode_list.appendChild(createLabel(button_channels_personalized));
-            mode_list.appendChild(createToggle('channels_personalized', data.channels_personalized, true));
+        mode_list.appendChild(createLabel(button_channels_personalized));
+        mode_list.appendChild(createToggle('channels_personalized', data.channels_personalized, true));
 
-            mode_list.appendChild(createLabel(button_channels_none));
-            mode_list.appendChild(createToggle('channels_none', data.channels_none, true));
-        }
+        mode_list.appendChild(createLabel(button_channels_none));
+        mode_list.appendChild(createToggle('channels_none', data.channels_none, true));
+    }
 
-        for (const input of document.querySelectorAll('input.checkbox')) {
-            input.addEventListener('change', () => {
-                chrome.storage.local.set({ [input.id]: input.checked });
-            });
-        }
-
-        document.querySelector('input#reset').addEventListener('click', () => {
-            for (const input of document.querySelectorAll('input.checkbox')) {
-                input.checked = input.getAttribute('default') === 'true';
-            }
-            chrome.storage.local.clear();
+    for (const input of document.querySelectorAll('input.checkbox')) {
+        input.addEventListener('change', () => {
+            chrome.storage.local.set({ [input.id]: input.checked });
         });
+    }
+
+    document.querySelector('input#reset').addEventListener('click', () => {
+        for (const input of document.querySelectorAll('input.checkbox')) {
+            input.checked = input.getAttribute('default') === 'true';
+        }
+        chrome.storage.local.clear();
     });
 });
