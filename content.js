16209899964a2577c62fe22e758572203102f446
Yudai the Tiny Developer <108601122+yudai-tiny-developer@users.noreply.github.com>
2023-06-15 21:42:40 +0900

    v1.20.1 (#207)
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -630,13 +630,6 @@ function main(common, lang) {
 
                 // continuation stopper
                 case 'YTD-CONTINUATION-ITEM-RENDERER':
-                    /*
-                    const div = document.createElement('div');
-                    div.classList.add('filter-separator');
-                    div.style.height;
-                    div.innerHTML = '-----';
-                    node.previousElementSibling.appendChild(div);
-                    */
                     node.previousElementSibling.classList.add('filter-separator');
                     break;
             }
