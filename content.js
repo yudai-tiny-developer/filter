89966978293f0f23662a4229a73dc4d5d2b3b340
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-01-07 22:14:28 +0900

    lang

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
-import(chrome.runtime.getURL('common.js')).then(common =>
-    import(chrome.runtime.getURL('lang/' + document.documentElement.getAttribute('lang') + '.js')).then(lang =>
-        main(common, lang)
-    )
-);
+import(chrome.runtime.getURL('common.js')).then(common => {
+    const lang = document.documentElement.getAttribute('lang');
+    import(chrome.runtime.getURL('lang/' + (lang ? lang : 'en') + '.js')).then(lang => {
+        main(common, lang);
+    });
+});
 
 function main(common, lang) {
     function updateButtonVisibility(node) {
