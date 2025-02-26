9f277d3b91e3e84f1c9772ea5e737e892c9ed1a0
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-26 23:32:09 +0900

    v1.43.2

--- content.js
+++ content.js
@@ -18,7 +18,7 @@ function main(app, common, lang) {
                 progress.appendChild(progress.querySelector('option.filter-button-progress.progress_all'));
 
                 for (const mode of common.order(data.order)) {
-                    if (mode === 'keyword' || mode === 'multiselection' || mode === 'responsive') {
+                    if (mode === 'keyword' || mode === 'multiselection' || mode === 'responsive' || mode === 'keyword_add_playlist' || mode === 'keyword_sidebar_channels' || mode === 'keyword_notification') {
                         // continue
                     } else if (mode.startsWith('progress_')) {
                         const option = progress.querySelector('option.filter-button-progress.' + mode);
@@ -102,6 +102,9 @@ function main(app, common, lang) {
             multiselection = common.value(data.multiselection, common.default_multiselection);
             responsive = common.value(data.responsive, common.default_responsive);
             limit = common.value(data.limit, common.defaultLimit);
+            keyword_add_playlist = common.value(data.keyword_add_playlist, common.default_keyword_add_playlist);
+            keyword_sidebar_channels = common.value(data.keyword_sidebar_channels, common.default_keyword_sidebar_channels);
+            keyword_notification = common.value(data.keyword_notification, common.default_keyword_notification);
             default_keyword = common.value(data.default_keyword, common.default_default_keyword);
 
             if (common.isSubscriptions(location.href)) {
@@ -394,8 +397,19 @@ function main(app, common, lang) {
             updateQueryRegex(browse, getActiveQuery());
             updateVisibility(browse);
 
-            for (const menu of app.querySelectorAll('form.filter-popup')) {
-                menu.style.display = display(keyword);
+            // add-playlist
+            for (const menu of app.querySelectorAll('form.filter-popup.filter-add-playlist')) {
+                menu.style.display = display(keyword_add_playlist);
+            }
+
+            // sidebar channels
+            for (const menu of app.querySelectorAll('form.filter-popup.filter-sidebar-channels')) {
+                menu.style.display = display(keyword_sidebar_channels);
+            }
+
+            // notification
+            for (const menu of app.querySelectorAll('form.filter-popup.filter-notification')) {
+                menu.style.display = display(keyword_notification);
             }
         });
     }
@@ -1096,49 +1110,55 @@ function main(app, common, lang) {
     }
 
     function insertPopupMenu(node) {
-        // add-to-playlist popup
-        for (const playlists of node.querySelectorAll('ytd-add-to-playlist-renderer div#playlists')) {
-            const parent = playlists.parentNode;
-            if (parent) {
-                const existsMenu = parent.querySelector('form.filter-popup');
-                if (existsMenu !== popupMenu.get(playlists)) {
-                    existsMenu?.remove();
-                    const menu = createPopupMenu(playlists, undefined);
-                    parent.insertBefore(menu, playlists);
+        // Save to playlist
+        if (keyword_add_playlist) {
+            for (const playlists of node.querySelectorAll('ytd-add-to-playlist-renderer div#playlists')) {
+                const parent = playlists.parentNode;
+                if (parent) {
+                    const existsMenu = parent.querySelector('form.filter-popup.filter-add-playlist');
+                    if (existsMenu !== popupMenu.get(playlists)) {
+                        existsMenu?.remove();
+                        const menu = createPopupMenu(playlists, undefined, 'filter-add-playlist');
+                        parent.insertBefore(menu, playlists);
+                    }
                 }
             }
         }
 
-        // notification popup
-        for (const items of node.querySelectorAll('yt-multi-page-menu-section-renderer div#items')) {
-            const parent = searchParentNode(items, 'YTD-MULTI-PAGE-MENU-RENDERER');
-            if (parent) {
-                const existsMenu = parent.querySelector('form.filter-popup');
-                if (existsMenu !== popupMenu.get(items)) {
-                    existsMenu?.remove();
-                    const menu = createPopupMenu(items, undefined);
-                    parent.insertBefore(menu, parent.querySelector('div#container') ?? parent.firstChild);
+        // Sidebar Subscriptions
+        if (keyword_sidebar_channels) {
+            for (const items of node.querySelectorAll('div#items:has(ytd-guide-entry-renderer#expander-item), div#items:has(ytd-guide-entry-renderer#collapser-item), div#expandable-items:has(ytd-guide-entry-renderer#expander-item), div#expandable-items:has(ytd-guide-entry-renderer#collapser-item)')) {
+                const parent = items.parentNode;
+                if (parent) {
+                    const existsMenu = parent.querySelector('form.filter-popup.filter-sidebar-channels');
+                    if (existsMenu !== popupMenu.get(items)) {
+                        existsMenu?.remove();
+                        const menu = createPopupMenu(items, 'ytd-guide-entry-renderer#expander-item', 'filter-sidebar-channels');
+                        parent.insertBefore(menu, items);
+                    }
                 }
             }
         }
 
-        // sidebar channels
-        for (const items of node.querySelectorAll('div#items:has(ytd-guide-entry-renderer#expander-item), div#items:has(ytd-guide-entry-renderer#collapser-item), div#expandable-items:has(ytd-guide-entry-renderer#expander-item), div#expandable-items:has(ytd-guide-entry-renderer#collapser-item)')) {
-            const parent = items.parentNode;
-            if (parent) {
-                const existsMenu = parent.querySelector('form.filter-popup');
-                if (existsMenu !== popupMenu.get(items)) {
-                    existsMenu?.remove();
-                    const menu = createPopupMenu(items, 'ytd-guide-entry-renderer#expander-item');
-                    parent.insertBefore(menu, items);
+        // Notification
+        if (keyword_notification) {
+            for (const items of node.querySelectorAll('yt-multi-page-menu-section-renderer div#items')) {
+                const parent = searchParentNode(items, 'YTD-MULTI-PAGE-MENU-RENDERER');
+                if (parent) {
+                    const existsMenu = parent.querySelector('form.filter-popup.filter-notification');
+                    if (existsMenu !== popupMenu.get(items)) {
+                        existsMenu?.remove();
+                        const menu = createPopupMenu(items, undefined, 'filter-notification');
+                        parent.insertBefore(menu, parent.querySelector('div#container') ?? parent.firstChild);
+                    }
                 }
             }
         }
     }
 
-    function createPopupMenu(container, expander) {
+    function createPopupMenu(container, expander, menu_class) {
         const menu = document.createElement('form');
-        menu.classList.add('filter-popup');
+        menu.classList.add('filter-popup', menu_class);
         menu.style.display = display(keyword);
 
         const input = createPopupQueryInput(menu);
@@ -1668,6 +1688,9 @@ function main(app, common, lang) {
     let multiselection = common.default_multiselection;
     let responsive = common.default_responsive;
     let limit = common.defaultLimit;
+    let keyword_add_playlist = common.default_keyword_add_playlist;
+    let keyword_sidebar_channels = common.default_keyword_sidebar_channels;
+    let keyword_notification = common.default_keyword_notification;
 
     const popupMenu = new Map();
 
--- popup.js
+++ popup.js
@@ -16,7 +16,8 @@ function main(common, settings, progress, data) {
     const settings_list_3 = document.body.querySelector('div#settings_list_3');
     const settings_list_4 = document.body.querySelector('div#settings_list_4');
     const settings_list_5 = document.body.querySelector('div#settings_list_5');
-    const settings_lists = [settings_list_1, settings_list_2, settings_list_3, settings_list_4, settings_list_5];
+    const settings_list_6 = document.body.querySelector('div#settings_list_6');
+    const settings_lists = [settings_list_1, settings_list_2, settings_list_3, settings_list_4, settings_list_5, settings_list_6];
 
     const progress_class = 'progress';
     const done_class = 'done';
@@ -26,7 +27,7 @@ function main(common, settings, progress, data) {
 
     settings.init(groups, settings_lists);
 
-    settings_list_1.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default));
+    settings_list_1.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default, 'header-main'));
     settings_list_1.appendChild(settings.createRow(data.button_label_live, common.button_label.live, 'live', data.live, true, data.default_live ? data.default_live : false, 'subscriptions', input => chrome.storage.local.set({ button_label_live: input.value }), common.button_label.clear));
     settings_list_1.appendChild(settings.createRow(data.button_label_streamed, common.button_label.streamed, 'streamed', data.streamed, true, data.default_streamed ? data.default_streamed : false, 'subscriptions', input => chrome.storage.local.set({ button_label_streamed: input.value }), common.button_label.clear));
     settings_list_1.appendChild(settings.createRow(data.button_label_video, common.button_label.video, 'video', data.video, true, data.default_video ? data.default_video : false, 'subscriptions', input => chrome.storage.local.set({ button_label_video: input.value }), common.button_label.clear));
@@ -35,7 +36,11 @@ function main(common, settings, progress, data) {
     settings_list_1.appendChild(settings.createRow(data.button_label_notification_on, common.button_label.notification_on, 'notification_on', data.notification_on, false, data.default_notification_on ? data.default_notification_on : false, 'subscriptions', input => chrome.storage.local.set({ button_label_notification_on: input.value }), common.button_label.clear));
     settings_list_1.appendChild(settings.createRow(data.button_label_notification_off, common.button_label.notification_off, 'notification_off', data.notification_off, false, data.default_notification_off ? data.default_notification_off : false, 'subscriptions', input => chrome.storage.local.set({ button_label_notification_off: input.value }), common.button_label.clear));
 
-    settings_list_2.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default));
+    settings_list_6.appendChild(settings.createRow(common.button_label.keyword_add_playlist, undefined, 'keyword_add_playlist', data.keyword_add_playlist, true));
+    settings_list_6.appendChild(settings.createRow(common.button_label.keyword_sidebar_channels, undefined, 'keyword_sidebar_channels', data.keyword_sidebar_channels, true));
+    settings_list_6.appendChild(settings.createRow(common.button_label.keyword_notification, undefined, 'keyword_notification', data.keyword_notification, true));
+
+    settings_list_2.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default, 'header-progress'));
     settings_list_2.appendChild(settings.createRow(data.button_label_progress_unwatched, common.button_label.progress_unwatched, 'progress_unwatched', data.progress_unwatched, true, data.default_progress_unwatched ? data.default_progress_unwatched : false, 'progress', input => chrome.storage.local.set({ button_label_progress_unwatched: input.value })));
     settings_list_2.appendChild(settings.createRow(data.button_label_progress_watched, common.button_label.progress_watched, 'progress_watched', data.progress_watched, true, data.default_progress_watched ? data.default_progress_watched : false, 'progress', input => chrome.storage.local.set({ button_label_progress_watched: input.value })));
 
@@ -47,7 +52,7 @@ function main(common, settings, progress, data) {
     settings_list_4.appendChild(settings.createRow(common.button_label.responsive, undefined, 'responsive', data.responsive, true));
     settings_list_4.appendChild(settings.createRow(common.button_label.limit, undefined, 'limit', data.limit, common.defaultLimit, undefined, undefined, input => chrome.storage.local.set({ limit: common.limit(input.value, common.defaultLimit, common.minLimit, common.maxLimit, common.stepLimit) }), undefined, 'step', common.minLimit, common.maxLimit, common.stepLimit, common.limit));
 
-    settings_list_5.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default));
+    settings_list_5.appendChild(settings.createHeaderRow(common.button_label.visibility, common.button_label.default, 'header-keyword'));
     settings_list_5.appendChild(settings.createRowKeyword(common.button_label.keyword, 'keyword', data.keyword, true, data.default_keyword, input => chrome.storage.local.set({ default_keyword: input.value }), common.button_label.clear));
 
     for (const settings_list of settings_lists) {
